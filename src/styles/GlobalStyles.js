7f7688618c2ac6b047e5c7c00e75ffce8c25bd52
hariyebk <harunbekri6@gmail.com>
2023-10-02 11:33:27 +0300

    fixed screen zoom level

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -127,7 +127,6 @@ font-size: 62.5%;
 body {
 font-family: "Poppins", sans-serif;
 color: var(--color-grey-700);
-
 transition: color 0.3s, background-color 0.3s;
 min-height: 100vh;
 line-height: 1.5;
